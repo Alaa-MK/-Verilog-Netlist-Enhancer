e68efc1d875a059356224ecb422397610d5fc158
Hadeel Mabrouk <hadeelmabrouk97@gmail.com>
2019-11-09 20:28:49 +0200

    Adding FFs Split Function

--- Netlist.py
+++ Netlist.py
@@ -155,6 +155,7 @@ class Netlist:
         I = '__i1__'
         O ='__o1__'
         #Filling slots for the input and the output
+        print (wires_dict)
         for w in wires_dict:
             if len(wires_dict[w]['destination']) == 0:
                 wires_dict[w]['destination'].append(O)
@@ -164,11 +165,12 @@ class Netlist:
                 wires_dict[w]['source']= I
                 Icount+=1
                 I = '__i{0}__'.format(Icount)
+                
         return wires_dict
+
     
     def _create_network(self):
         wires_dict = self._get_wires_dict()
-        
         #Creating the edge list of the circuit
         for w in wires_dict:
             for d in wires_dict[w]['destination']:
@@ -177,23 +179,34 @@ class Netlist:
     
     def _add_delay_to_graph(self):
         wires_dict = self._get_wires_dict()
-        for w in wires_dict:
-            for d in wires_dict[w]['destination']:
-                if wires_dict[w]['destination'][0:2]!='__':
-                    print(self.netlist[d])
-                    s = wires_dict[w]['source']
+        for key, value in wires_dict.items():
+            for d in value['destination']:
+                if d[0:2]!='__':
+                    #print(self.netlist[d])
+                    s = value['source']
                     d_cell_type = self.netlist[d]['type']
                     d_cell_cap = self.netlist[d]['load_capacitance']
                     d_pin_name=''
-                    for key, value in list(self.netlist[d].items())[1:-2]:
-                        if value==w:
-                            d_pin_name = key
+                    for k2, v2 in list(self.netlist[d].items())[1:-2]:
+                        if v2==key:
+                            d_pin_name = k2
                     self.g[s][d]['weight']= self.liberty.get_pin_delay(d_cell_type, d_pin_name, d_cell_cap)
 
     def create_graph(self):
         self._create_network()
-        self._add_delay_to_graph()
+        #self._add_delay_to_graph()
+        #self.split_on_FFs()
         return self.g
+
+    def split_on_FFs(self):
+        countq=0
+        for e in self.g.edges:
+            if(e[0][0:2]!='__'):
+                if(self.netlist[e[0]]['type'][0:3]=='DFF'):
+                    self.g.add_edge('q'+str(countq),e[1])
+                    countq+=1
+                    self.g.remove_edge(e)
+
     
     def get_all_delays(self):
         self.create_graph()
