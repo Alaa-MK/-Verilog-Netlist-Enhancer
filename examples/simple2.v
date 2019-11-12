ff2ed7699d9a521b7f3bd699999a3500e6f6f0ed
Hadeel Mabrouk <hadeelmabrouk97@gmail.com>
2019-11-12 23:55:40 +0200

    Update simple2.v

--- examples/simple2.v
+++ examples/simple2.v
@@ -1,7 +1,9 @@
 module simple (
 inp1,
 inp2,
-out
+out,
+out2,
+out3
 );
 
 // Start PIs
@@ -10,6 +12,8 @@ input inp2;
 
 // Start POs
 output out;
+output out2;
+output out3;
 
 // Start wires
 wire n1;
@@ -19,11 +23,15 @@ wire n4;
 wire inp1;
 wire inp2;
 wire out;
+wire out2;
+wire out3;
 
 // Start cells
 NAND2X1 u1 ( .A(inp1), .B(inp2), .Y(n1) );
-NOR2X1 u5 (.A(n1), .B(inp1), .Y(n2));
-XOR2X1 u6 (.A(n2), .B(n1), .Y(n3));
+NOR2X1 u5 ( .A(n1), .B(inp1), .Y(n2) );
+INVX1 u7 ( .A(n2), .Y(out2) );
+INVX1 u8 ( .A(n2), .Y(out3) );
+XOR2X1 u6 ( .A(n2), .B(n1), .Y(n3) );
 INVX1 u2 ( .A(n3), .Y(n4) );
 INVX1 u3 ( .A(n4), .Y(out) );
 
